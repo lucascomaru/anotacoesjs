a46dfef06ddba77f8b47d29aabff5b0dc33f3f66
Lucas Comaru <lucascomaru20@gmail.com>
2023-06-04 10:14:51 -0300

    Mais ex do curso

---
+++ desafiosjs/desafio5.js
@@ -0,0 +1,9 @@
+function verificarDivisivel(num1, num2){
+    if(num1 % num2 ===0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(verificarDivisivel(4, 2))
---
+++ desafiosjs/desafio8.js
@@ -0,0 +1,24 @@
+function calculadora(num1, operacao, num2){
+    resultado = 0
+    switch (operacao) {
+        case '+':
+            resultado = num1 + num2
+            break;
+        case '-':
+            resultado = num1 - num2
+            break;
+        case '*':
+            resultado = num1 * num2
+            break;
+        case '/':
+            resultado = num1 / num2
+            break;
+    
+        default:
+            console.log('Essa operação não está disponível')
+            break;
+    }
+    return resultado
+}
+
+console.log(calculadora(4,'/', 5))
