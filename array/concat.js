be573465fcdbe21a8ff9fec925c2fb7eac39b2b7
Lucas <lucascomaru20@gmail.com>
2023-06-09 15:00:37 -0300

    Aulas e ex

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valesca', 'Cibalena'];
+const filhos = ['Alex', 'Carlos'];
+const todos = filhas.concat(filhos, 'Fulano');
+console.log(todos, filhas, filhos);
+
+console.log([].concat([1,3], [3,4], [5,6]));
--- array/reduce1.js
+++ array/reduce1.js
@@ -8,6 +8,6 @@ const alunos = [
 const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual){
     console.log(acumulador, atual)
     return acumulador + atual
-}, 0)
+}, 0);
 
-console.log(resultado)
+console.log(resultado);
