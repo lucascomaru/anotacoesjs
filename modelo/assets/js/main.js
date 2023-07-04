70a54b110a1d3781d36269243e415fba7a17c289
Lucas <lucascomaru20@gmail.com>
2023-07-03 23:30:17 -0300

    Novas aulas

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -9,9 +9,23 @@ function calculadoraIMC() {
 
         const calcularIMC = pesoInput / (alturaInput * alturaInput);
 
+        if (calcularIMC < 18.5) {
+            resposta = 'Você está abaixo do peso';
+        } else if (calcularIMC >= 18.5 && calcularIMC <= 24.9) {
+            resposta = 'Peso normal';
+        } else if (calcularIMC >= 25 && calcularIMC <= 29.9) {
+            resposta = 'Sobrepeso';
+        } else if (calcularIMC >= 30 && calcularIMC <= 34.9) {
+            resposta = 'Obesidade grau 1';
+        } else if (calcularIMC >= 35 && calcularIMC <= 39.9) {
+            resposta = 'Obesidade grau 2';
+        } else if (calcularIMC >= 40) {
+            resposta = 'Obesidade grau 3';
+        }
+
         resultado.innerHTML = `<div>Seu IMC é de ${calcularIMC.toFixed(
             2
-        )}</div>`;
+        )} - (${resposta})</div>`;
     }
 
     const botao = document.getElementById('botao');
